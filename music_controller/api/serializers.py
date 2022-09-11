dc52743a3274f67a99f2ccf40d20d47b480f33e8
FPLTomeczek <tomaszs18m@gmail.com>
2022-09-11 23:27:19 +0200

    p3

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from distutils.command.build_scripts import first_line_re
+import imp
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
